a360e586baeb44a7ea071c55fd02a60323ae06b5
Johnny Hu <johnny@comradeagency.com>
2018-03-19 16:57:14 -0700

    Update track acomponent to render audio features and analysis

--- src/app/components/track/track.component.ts
+++ src/app/components/track/track.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TrackComponent implements OnInit {
 
   @Input('trackAudioFeatures') trackAudioFeatures: any[];
+  @Input('trackAudioAnalysis') trackAudioAnalysis: any[];
+
   featuresList = [
     'acousticness',
     'danceability',
